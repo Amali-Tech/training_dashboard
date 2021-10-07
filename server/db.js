2747ed7f125aa5880edb040ec949996dd5e71d02
John Jebo <john.jebo@amalitech.com>
2021-10-07 12:17:59 +0000

    envs

--- server/db.js
+++ server/db.js
@@ -1,14 +1,14 @@
-require('dotenv').config({ path: '../.env' });
+require("dotenv").config(); 
 const Pool = require('pg').Pool;
 
-// console.log(process.env)
+console.log(process.env.host)
 
 const pool = new Pool ({
-    user: process.env.user,
-    password: process.env.password,
-    host: process.env.host,
-    port: process.env.port,
-    database: process.env.database
+    user: process.env.user || 'potsgres',
+    password: process.env.password || 'amalitech',
+    host: process.env.host || 'aegis-live.ck0dwzfc16xf.eu-west-1.rds.amazonaws.com',
+    port: process.env.port || '5432',
+    database: process.env.database || 'training_dashboard'
 });
 
 module.exports = pool;
