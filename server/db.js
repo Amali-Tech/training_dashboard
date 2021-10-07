df7f33614e6f0ba4fef77218f568e9502760ed57
John Jebo <john.jebo@amalitech.com>
2021-10-07 11:18:19 +0000

    removed db connection port

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
 require('dotenv').config({ path: '../.env' });
 const Pool = require('pg').Pool;
 
+// console.log(process.env)
+
 const pool = new Pool ({
     user: process.env.user,
     password: process.env.password,
     host: process.env.host,
-    port: process.env.port,
     database: process.env.database
 });
 
